77a00e19cac93c77386678fc0c0f0c34847b5747
vvnikolenko <nikolenko.vitalii@gmail.com>
2025-05-12 19:33:17 +0200

    Float logic added

--- ConsoleApplication1/main.cpp
+++ ConsoleApplication1/main.cpp
@@ -1,6 +1,10 @@
+#include <cstdio>
+
 int main(int argc, char const *argv[])
 {
-	for (int i =0;i<5;++i)
+	float k = 14.7;
+	k -= 5;
+	for (int i =0;i<(int)k;++i)
 	{ printf("*");}
 	/* code */
 	return 0;
